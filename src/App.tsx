9e5c7d81c633a1c7c9219ae20779bd50922f529e
lucas paiva <lucas.paiva@mundiale.com.br>
2023-01-02 18:29:19 -0300

    fix:creating header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Header } from './components/Header';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
+
+
+
